6369f6470b4c68359830f37aac84b08ed130c558
kishan47sinha <160162105+kishan47sinha@users.noreply.github.com>
2026-07-29 17:27:47 +0530

    Add files via upload

---
+++ Query/query1.sql
@@ -0,0 +1,6 @@
+-- Identify the highest-priced pizza.
+
+SELECT pizza_types.name, pizzas.price
+from pizza_types join pizzas
+on pizzas.pizza_type_id = pizza_types.pizza_type_id
+order by pizzas.price desc limit 1;
